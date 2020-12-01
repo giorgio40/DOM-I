dc5acb65678c67161a00d56a48de2c558a87b2bf
george-hatzigeorgio <georgehatzigeorgio@zohomail.com>
2020-11-30 19:06:22 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,65 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const title = document.querySelector("title")
+title.textContent = "GREAT IDEA"
+
+const links = document.querySelector("nav")
+const link1 = document.querySelector("a")
+link1.textContent = "Services"
+const link2 = link1.nextElementSibling;
+link2.textContent = "Product"
+const link3 = link2.nextElementSibling;
+link3.textContent = "Vision"
+const link4 = link3.nextElementSibling;
+link4.textContent= "Features"
+const link5 = link4.nextElementSibling;
+link5.textContent = "About"
+const link6 = link5.nextElementSibling;
+link6.textContent = "Contact"
+link1.href ="#"
+link2.href ="#"
+link3.href="#"
+link4.href = "#"
+link5.href = "#"
+link6.href = "#"
+
+const heading = document.querySelector("h1")
+heading.textContent= "Dom is Awsome"
+const button = heading.nextElementSibling;
+button.textContent = "Get Started"
+
+const div1 = document.querySelector(".text-content")
+const hd = div1.querySelector("h4");
+hd.textContent = "Features"
+const parag = div1.querySelector("p")
+parag.textContent ="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eou ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis"
+
+// div1.classList.add("second")
+// const headin  = document.querySelectorAll("h4"[1])
+
+const div2 = div1.nextElementSibling;
+const headingTwo = div2.querySelector("h4")
+
+headingTwo.textContent ="About"
+const paragraph = headingTwo.nextElementSibling;
+paragraph.textContent ="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis."
+
+
+const hdn = document.querySelectorAll("h4")[2]
+
+hdn.textContent = "Services"
+
+const para = hdn.nextElementSibling;
+para.textContent ="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const heading4 = document.querySelectorAll("h4")[3]
+heading4.textContent = "Product"
+const paragr = heading4.nextElementSibling;
+paragr.textContent ='Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const heading5 = document.querySelectorAll("h4")[4]
+heading5.textContent = "Vision"
+const lastparagraph = heading5.nextElementSibling;
+lastparagraph.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
